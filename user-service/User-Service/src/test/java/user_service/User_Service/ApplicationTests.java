97e9b7b5e52ec678511e9802c85aa2607310661e
matsatsi <matsatsimasetla@gmail.com>
2026-03-16 19:33:51 +0200

    Initial project architecture for secure finance microservices platform

---
+++ user-service/User-Service/src/test/java/user_service/User_Service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package user_service.User_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
